36f3b17a7988373cc27c969cad4514219a86e5dd
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-06-25 18:08:42 +0700

    added simple tests

---
+++ src/test/java/qa/guru/simpleTests/SuccessfulTests.java
@@ -0,0 +1,32 @@
+package qa.guru.simpleTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SuccessfulTests {
+    @Test
+    void test1(){
+        assertTrue(true);
+    }
+
+    @Test
+    void test2(){
+        assertTrue(true);
+    }
+
+    @Test
+    void test3(){
+        assertTrue(true);
+    }
+
+    @Test
+    void test4(){
+        assertTrue(true);
+    }
+
+    @Test
+    void test5(){
+        assertTrue(true);
+    }
+}
