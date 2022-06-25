73224d009ea6c6d1645d86bcc222331241035c80
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-06-25 21:25:32 +0700

    added successfulTestPracticeForm without Selenoid

---
+++ src/test/java/qa/guru/simpleTests/demoqa/TestBase.java
@@ -0,0 +1,24 @@
+package qa.guru.simpleTests.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("enableVNC", true);
+//        capabilities.setCapability("enableVideo", true);
+//        Configuration.browserCapabilities = capabilities;
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1519x800";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+}
